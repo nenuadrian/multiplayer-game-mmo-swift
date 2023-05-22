6a69fc2138e68032f4e39603192d7160e59b1aa7
Nenu Adrian <adrian.nenu@gmail.com>
2023-05-22 11:15:07 +0100

    Upgrade

--- Client/Package.swift
+++ Client/Package.swift
@@ -1,12 +1,29 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Client",
-    targets: [],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Client",
+            targets: ["Client"]),
+    ],
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/BlueSocket", majorVersion: 0, minor: 12),
-        .Package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", majorVersion: 0),
-        .Package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", majorVersion: 1)
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.0.1"),
+        .package(url: "https://github.com/IBM-Swift/BlueSocket", from: "0.0.1"),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "0.0.1"),
+        .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", from: "0.0.1")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Client"),
+        .testTarget(
+            name: "ClientTests",
+            dependencies: ["Client"]),
     ]
 )
