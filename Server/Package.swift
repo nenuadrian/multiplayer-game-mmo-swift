c06cd7ae1e0cee89da4edb84b539702c84010d36
Nenu Adrian <adrian.nenu@gmail.com>
2023-05-23 11:05:23 +0100

    f

--- Server/Package.swift
+++ Server/Package.swift
@@ -19,8 +19,8 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "Server"),
+        .executableTarget(
+            name: "Server", dependencies: ["Server"]),
         .testTarget(
             name: "ServerTests",
             dependencies: ["Server"]),
