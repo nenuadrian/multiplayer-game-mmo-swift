2d396203a03b1f01c756b3860be3c51e301d60f0
Nenu Adrian <adrian.nenu@gmail.com>
2023-05-23 11:08:39 +0100

    f

--- Client/Package.swift
+++ Client/Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "Client", dependencies: ["Client"]),
+            name: "Client", dependencies: ["Socket"]),
         .testTarget(
             name: "ClientTests",
             dependencies: ["Client"]),
