ab4e23b486b32566f6ca30840213bee002747dac
Nenu Adrian <adrian.nenu@gmail.com>
2023-05-25 14:14:15 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+sudo apt update
+
+sudo apt install clang libpython2.7 libpython2.7-dev
+
+wget https://swift.org/builds/swift-5.7-release/ubuntu2004/swift-5.7-RELEASE/swift-5.7-RELEASE-ubuntu20.04.tar.gz
+
+tar xzf swift-5.7-RELEASE-ubuntu20.04.tar.gz
+
+sudo mv swift-5.3-RELEASE-ubuntu20.04 /usr/share/swift
