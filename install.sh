6a625973e77f78c5ee2f6fbf40fbe1b561b88fc7
Nenu Adrian <adrian.nenu@gmail.com>
2023-05-25 13:19:27 +0000

    install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,18 @@
 sudo apt update
 
-sudo apt install clang libpython2.7 libpython2.7-dev
+sudo apt -y install clang libpython2.7 libpython2.7-dev
 
 wget https://swift.org/builds/swift-5.7-release/ubuntu2004/swift-5.7-RELEASE/swift-5.7-RELEASE-ubuntu20.04.tar.gz
 
 tar xzf swift-5.7-RELEASE-ubuntu20.04.tar.gz
 
-sudo mv swift-5.3-RELEASE-ubuntu20.04 /usr/share/swift
+rm swift-5.7-RELEASE-ubuntu20.04.tar.gz
+
+sudo mv swift-5.7-RELEASE-ubuntu20.04 /usr/share/swift
+
+echo "export PATH=/usr/share/swift/usr/bin:$PATH" >> ~/.zshrc
+
+zsh
+
+source ~/.zshrc
+
